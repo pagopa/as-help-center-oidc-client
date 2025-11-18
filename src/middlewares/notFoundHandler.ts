a85eef42b65d62b9ff8f9620f565d20ea3a82f9b
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-11-18 11:08:01 +0100

    feat(error handling): not found redirect adaption

--- src/middlewares/notFoundHandler.ts
+++ src/middlewares/notFoundHandler.ts
@@ -1,8 +1,9 @@
 import { ApiError } from '@errors/ApiError';
+import { ERROR_CODES } from '@utils/constants';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 // Route not found - 404 handler middleware
-export function notFoundHandler(_req: Request, _res: Response, next: NextFunction) {
-  next(new ApiError('Route not found', StatusCodes.NOT_FOUND));
+export function notFoundHandler(req: Request, res: Response, next: NextFunction) {
+  next(new ApiError('Route not found: ' + req.originalUrl, StatusCodes.NOT_FOUND, ERROR_CODES.NOT_FOUND_ERROR));
 }
