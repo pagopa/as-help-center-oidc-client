f49bcc594831be8c0c338bcfe8409c3546c4328c
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-11-24 12:41:29 +0100

    feat: async lambda wrapper with env vars loading from parameter store

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,26 @@
-import type { Handler } from 'aws-lambda';
 import serverlessExpress from '@codegenie/serverless-express';
-import app from './app';
+import { loadParametersIntoEnv } from './utils/parameterStore';
 
-// wraps express app to work with lambda and api gtw
-export const lambdaHandler: Handler = serverlessExpress({ app });
+let serverlessExpressInstance: any = null;
+
+// Initialize Lambda handler once per lambda lifecycle
+async function setup(event: any, context: any) {
+  await loadParametersIntoEnv();
+
+  // Import app only after getting env vars, so the app reads correct env values
+  const mod = await import('./app');
+  const app = mod.default;
+
+  // Wrap express app to work with lambda and api gtw
+  serverlessExpressInstance = serverlessExpress({ app });
+  return serverlessExpressInstance(event, context);
+}
+
+export const lambdaHandler = (event: any, context: any) => {
+  // If already initialized, use the ready handler
+  if (serverlessExpressInstance) {
+    return serverlessExpressInstance(event, context);
+  }
+
+  return setup(event, context);
+};
