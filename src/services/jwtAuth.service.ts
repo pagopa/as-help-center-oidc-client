b1851125d48370372c924124d9b45755f80d0bcc
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2026-01-08 15:53:46 +0100

    feat(CACI-499): remove 'TINIT-' prefix from fiscalNumber when present

--- src/services/jwtAuth.service.ts
+++ src/services/jwtAuth.service.ts
@@ -5,13 +5,16 @@ import { v4 as uuidv4 } from 'uuid';
 const JWT_SIGN_ALGORITHM = 'HS256';
 
 export function generateAuthJwt(name: string, org: string, emailContact: string, fiscalNumber?: string) {
+  const normalizedFiscalNumber =
+    fiscalNumber && fiscalNumber.startsWith('TINIT-') ? fiscalNumber.substring('TINIT-'.length) : fiscalNumber;
+
   const payload = {
     iat: Math.floor(new Date().getTime() / 1000),
     jti: uuidv4(),
     name: name,
     email: emailContact,
     organization: org,
-    user_fields: { aux_data: fiscalNumber },
+    user_fields: { aux_data: normalizedFiscalNumber },
   };
   // encode
   // expiring is automatically set by zendesk, but it works only after an inactivity period (and can be changed - from 5 minutes to 2 weeks)
